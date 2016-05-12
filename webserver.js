e39816f4172abe8569b0fdf4696ca9ecbab5e88e
Juan Ferreras <juan@flioh.com>
2016-05-12 14:26:35 -0300

    Prepared app for heroku

--- webserver.js
+++ webserver.js
@@ -5,12 +5,13 @@ var rutaProyectosAPI = require('rutas/proyecto');
 var rutaTareasAPI = require('rutas/tarea');
 
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public_html'));
 app.use('/clientes', rutaClientesAPI);
 app.use('/proyectos', rutaProyectosAPI);
 app.use('/tareas', rutaTareasAPI);
 
-app.listen(8080, function(){
+app.listen(port, function(){
   console.log("Express server listening en puerto 8080");
 });
